de9ac042852302d43bb23ded74819ff0f249e8f1
wilgustavo <wilgustavo@gmail.com>
2019-07-03 12:10:55 -0300

    Crear base de datos para probar la aplicacion

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+insert into contacto 
+values 
+(213, 'Rogelio Salmona', '1967-12-06', 'rsalmon@email.com',  null, 'Av.San Matin 2321 1C', 11809),
+(219, 'Marina Waisman', '1940-04-29', 'waisman@email.com',  null, null, 11809),
+(222, 'Antonio Ochoa', '1943-09-18', 'ochoaaa@hotmail.com',  '021-41202243', 'Callao 32', 12202),
+(354, 'Cesar Pelli', '1987-05-01', 'pellic@gmail.com',  '030-45544332', 'Viamonte 3211 3A', 12202),
+(355, 'Ruth Rivera', '1987-12-09', 'rivera@gmail.com',  '055-5554430', 'Mexico 802', 13013),
+(361, 'Rafael Viñoly', '1963-10-12', 'virafa@email.com',  '0121-1344452', 'Caracas 1237', 12202),
+(362, 'Carmen Córdova', '1990-09-18', 'china@email.com',  null, null, 12202),
+(363, 'Glenda Kapstein', '1979-04-29', 'kapstein@gmail.com',  '4211211', 'Av.9 de Julio 211', 13503),
+(364, 'Tatiana Bilbao', '1992-07-07', 'bilbaota@hotmail.com',  null, 'Chaco y Pres.Peña', 14003);
+
+insert into localidad
+values
+(11809, '1021', 'LURO', 'Bs.As.' ),
+(12104, '4051', 'CHASCOMUS', 'Bs.As.' ),
+(12202, '3220', 'CHIVILCOY', 'Bs.As.' ),
+(12507, '8921', 'PEHUEN-CO', 'Bs.As.' ),
+(12702, '4728', 'CORONEL PRINGLES', 'Bs.As.' ),
+(12803, '3290', 'CORONEL SUAREZ', 'Bs.As.' ),
+(12909, '4187', 'MUNRO', 'Bs.As.' ),
+(13013, '6611', 'SAN JUSTO', 'Bs.As.' ),
+(13309, '1011', 'LOS CARDALES', 'Bs.As.' ),
+(13503, '9990', 'MIRAMAR', 'Bs.As.' ),
+(14003, '4070', 'ZARATE', 'Bs.As.' ),
+(14307, '1001', 'MAR DE TUYU', 'Bs.As.' ),
+(14410, '1004', 'VILLA GESELL', 'Bs.As.' );
+	
